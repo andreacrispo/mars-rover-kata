2858a4a1f2933ea0b4075ca5894ad1e4abfd79e9
Andrea Crispo <andrea.crispo.cr@gmail.com>
2020-11-01 14:34:37 +0100

    Init empty project

---
+++ src/test/java/my/playground/AppTest.java
@@ -0,0 +1,13 @@
+package my.playground;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void test() {
+
+    }
+}
